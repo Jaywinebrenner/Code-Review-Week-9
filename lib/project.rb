551b4bb3541333887e367fde919e4441d9394d11
Jay Winebrenner <jaywinebrenner@gmail.com>
2020-01-10 09:10:03 -0800

    reorganize data base naming struction, change code accordingly

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,8 @@
 class Project
-  attr_accessor :name, :id
+  attr_accessor :title, :id
 
   def initialize(attributes)
-    @name = attributes.fetch(:name)
+    @title = attributes.fetch(:title)
     @id = attributes.fetch(:id)
   end
 
@@ -10,21 +10,21 @@ class Project
     returned_projects = DB.exec("SELECT * FROM projects;")
     projects = []
     returned_projects.each() do |project|
-      name = project.fetch("name")
+      title = project.fetch("title")
       id = project.fetch("id").to_i
-      projects.push(Project.new({:name => name, :id => id}))
+      projects.push(Project.new({:title => title, :id => id}))
     end
     projects
   end
 
   def save
-    result = DB.exec("INSERT INTO projects (name) VALUES ('#{@name}') RETURNING id;")
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
     @id = result.first().fetch("id").to_i
   end
 
   def ==(project_to_compare)
     if project_to_compare != nil
-      self.name() == project_to_compare.name()
+      self.title() == project_to_compare.title()
     else
       false
     end
@@ -37,29 +37,29 @@ class Project
   def self.find(id)
     project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
     if project
-      name = project.fetch("name")
+      title = project.fetch("title")
       id = project.fetch("id").to_i
-      Project.new({:name => name, :id => id})
+      Project.new({:title => title, :id => id})
     else
       nil
     end
   end
 
   def self.sort
-    # lowercasing 'name' first allows proper alphabetization, i.e. 'Black' comes before 'apple' if not lowercased because of character value
-    sorted_projects = DB.exec("SELECT * FROM projects ORDER BY lower(name);")
+    # lowercasing 'title' first allows proper alphabetization, i.e. 'Black' comes before 'apple' if not lowercased because of character value
+    sorted_projects = DB.exec("SELECT * FROM projects ORDER BY lower(title);")
     projects = []
     sorted_projects.each() do |project|
-      name = project.fetch("name")
+      title = project.fetch("title")
       id = project.fetch("id").to_i
-      projects.push(Project.new({:name => name, :id => id}))
+      projects.push(Project.new({:title => title, :id => id}))
     end
     projects
   end
 
-  def update(name)
-    @name = name
-    DB.exec("UPDATE projects SET name = '#{@name}' WHERE id = #{@id}")
+  def update(title)
+    @title = title
+    DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id}")
   end
 
   def delete
